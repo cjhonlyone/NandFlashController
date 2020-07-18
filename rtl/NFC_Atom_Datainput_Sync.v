ed85dc05afbf4c552aa42ea31b75c508dc6fc750
Cao jiahui <cjh416593819@gmail.com>
2020-07-18 19:40:09 +0800

    initial commit

--- rtl/NFC_Atom_Datainput_Sync.v
+++ rtl/NFC_Atom_Datainput_Sync.v
@@ -230,7 +230,7 @@ module NFC_Atom_Datainput_Sync
                     
                     rChipEnable         <= { 2*NumberOfWays{1'b0} };
                     rReadEnable         <= 4'b0011;
-                    rWriteEnable        <= 4'b0000;
+                    rWriteEnable        <= 4'b0001;
                     rAddressLatchEnable <= 4'h0;
                     rCommandLatchEnable <= 4'h0; 
 
--- rtl/NFC_Atom_Idle.v
+++ rtl/NFC_Atom_Idle.v
@@ -6,6 +6,8 @@ module NFC_Atom_Command_Idle
     parameter NumberOfWays    =   4
 )
 (
+    iTargetWay              ,
+
     oDQSOutEnable           ,
     oDQOutEnable            ,
     oDQStrobe               ,
@@ -16,6 +18,8 @@ module NFC_Atom_Command_Idle
     oAddressLatchEnable     ,
     oCommandLatchEnable  
 );
+    input   [NumberOfWays - 1:0]    iTargetWay              ;
+
     output                          oDQSOutEnable           ;
     output                          oDQOutEnable            ;
 
@@ -32,7 +36,7 @@ module NFC_Atom_Command_Idle
 
     assign oDQStrobe           = 8'h0                       ;   
     assign oDQ                 = 32'h0000                   ;   
-    assign oChipEnable         = { 2*NumberOfWays{1'b0} }   ;   
+    assign oChipEnable         = { iTargetWay,iTargetWay }  ;   
     assign oReadEnable         = 4'b0011                    ;   
     assign oWriteEnable        = 4'b0000                    ;   
     assign oAddressLatchEnable = 4'h0                       ;   
