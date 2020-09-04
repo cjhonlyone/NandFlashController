d6501facaf31d83b8062d2b23ac4dfd3e5389c98
Cao jiahui <cjh416593819@gmail.com>
2020-09-04 22:28:18 +0800

    Update Project and Software

--- prj/ZynqNandPrj/BuildZynqPrj.tcl
+++ prj/ZynqNandPrj/BuildZynqPrj.tcl
@@ -128,7 +128,7 @@ if {[string equal [get_filesets -quiet sources_1] ""]} {
 
 # Set IP repository paths
 set obj [get_filesets sources_1]
-set_property "ip_repo_paths" "[file normalize "$origin_dir/../ip"]" $obj
+set_property "ip_repo_paths" "[file normalize "$origin_dir/../../ip"]" $obj
 
 # Rebuild user ip_repo's index before adding any source files
 update_ip_catalog -rebuild
